82d5c8689d5c98880134d1876f6ad0e40d237cdc
speedan <malmborg_91@hotmail.com>
2025-06-25 16:36:43 +0200

    created a new file which accounts for the chunking and indexing part, made function which chunks the transcript into couple of exchanges with the metadata

---
+++ src/chunking_indexing.py
@@ -0,0 +1,54 @@
+import json
+from data_processing import load_json_data
+
+# --- Configuration (path) ---
+json_file_path = "src/data/oscar_transformed_data.json"
+
+
+# --- Functions ---
+def chunking_data(json_file_path: str):
+    
+    json_data = load_json_data(json_file_path)
+
+    final_structured_chunks = []
+
+    for msg in json_data:
+        chunks_list = []
+        customer_response_count = 0
+
+        full_transcript = msg.get("transcript")
+        metadata = msg.get("metadata")
+
+        chunked_transcript_list = full_transcript.split('\n')
+        
+        for chunk in chunked_transcript_list:
+            chunks_list.append(chunk)
+
+            if 'customer:' in chunk:
+                customer_response_count += 1
+
+            
+            if customer_response_count == 4:
+                # 1) Combine the collected lines into a single text block
+                final_chunk_text = "\n".join(chunks_list)
+                
+                # 2) Structure the final chunk with separate text and metadata
+                final_structured_chunks.append({
+                    "chunked_text": final_chunk_text,
+                    "metadata": metadata
+                    })
+                
+                # 3) Reset for the next chunk
+                chunks_list = [] 
+                customer_response_count = 0
+                
+    if final_structured_chunks:
+        final_chunk_text = "\n".join(chunks_list)
+        final_structured_chunks.append({
+            "chunk_text": final_chunk_text,
+            "metadata": metadata
+        })
+    
+    print(json.dumps(final_structured_chunks[2], indent=2))
+
+chunking_data(json_file_path)
