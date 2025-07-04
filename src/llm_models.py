2730af3dd1aaaa9f7dc6918eb29eb35bed847e09
speedan <malmborg_91@hotmail.com>
2025-07-04 12:47:09 +0200

    adjusted the code for every llm since the api documentation was updated, now I get a response as usual

--- src/llm_models.py
+++ src/llm_models.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-import google.generativeai as genai
+#import google.generativeai as genai
+from google import genai
 
 
 def get_gemini_response(prompt: str) -> str:
@@ -12,11 +13,17 @@ def get_gemini_response(prompt: str) -> str:
     if not api_key:
         raise ValueError("GEMINI_API_KEY not found in environment variables.")
 
-    genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-2.5-flash')
+    client = genai.Client(api_key=api_key)
 
-    response = model.generate_content(prompt)
+    response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents=prompt)
+    
+    if response.text is None:
+        return "Error: No response text generated"
+    
     return response.text
+    
 
 
 def gemini_1_5_flash_8b_reponse(prompt: str) -> str:
@@ -28,9 +35,36 @@ def gemini_1_5_flash_8b_reponse(prompt: str) -> str:
     if not api_key:
         raise ValueError("GEMINI_API_KEY not found in environment variables.")
 
-    genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash-8b')
-    #model_name = model.model_name[7::] # Further down when I want to adjust for dynamic filenames
+    client = genai.Client(api_key=api_key)
 
-    response = model.generate_content(prompt)
+    response = client.models.generate_content(
+    model='gemini-1.5-flash-8b',
+    contents=prompt)
+    
+    if response.text is None:
+        return "Error: No response text generated"
+    
     return response.text
+
+
+def gemini_2_5_flash_lite_preview_reponse(prompt: str) -> str:
+    """
+    Sends a prompt to the Gemini model and returns the response.
+    """
+    load_dotenv()
+    api_key = os.getenv('GEMINI_API_KEY')
+    if not api_key:
+        raise ValueError("GEMINI_API_KEY not found in environment variables.")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+    model='gemini-2.5-flash-lite-preview-06-17',
+    contents=prompt)
+    
+    if response.text is None:
+        return "Error: No response text generated"
+    
+    return response.text
+
+
